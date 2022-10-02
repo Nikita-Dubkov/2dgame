d2346756a3f03f7ac5e08af80e0df9710e3c8b80
NikitaPC <pc@nikita.com>
2022-10-02 18:51:36 +0300

    OpenGL and GLFW initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,51 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);//Area where do we wanna draw 
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
 
 int main(void)
 {
-    GLFWwindow* window;
-
+    
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfInit failed!" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "2dGame", nullptr , nullptr);
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow failed" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow);
 
     if(!gladLoadGL())
     {
@@ -28,23 +54,28 @@ int main(void)
         return -1;
     }
     
-    std::cout <<"OpenGL" << " " << GLVersion.major << "." << GLVersion.minor << std::endl;
-
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    
     glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        /* Swap front and back buffers */ 
+        //Renderer draw a buffer,frame after next monitor update buffer show at the monitor
+        //2 buffers : front and back, Randerer draw at the back buffer,monitor show only front buffer
+        //When we used glfwSwapBuffer buffers swaps,but the frame at the back buffer are finished and
+        //the image from back buffer cannot be changed at the next timeframe
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    //Releasing resouses
     glfwTerminate();
     return 0;
 }
