5722e7e268e9d60411aa377599715e22a53505a7
NikitaPC <pc@nikita.com>
2022-10-27 22:31:10 +0300

    add Resource Manager

--- src/Renderer/ShaderProgram.cpp
+++ src/Renderer/ShaderProgram.cpp
@@ -1,92 +1,94 @@
 #include "ShaderProgram.h"
+
 #include <iostream>
 
-namespace Renderer
-{
-	ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
-	{
-		GLuint vertexShaderID;
-		if (!createShader(vertexShader, GL_VERTEX_SHADER, vertexShaderID))
-		{
-			std::cout << "VERTEX SHADER compile-time error" << std::endl;
-			return;
-		}
-		GLuint fragmentShaderID;
-		if (!createShader(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderID))
-		{
-			std::cout << "FRAGMENT SHADER compile-time error" << std::endl;
-			glDeleteShader(vertexShaderID);
-			return;
-		}
+namespace Renderer {
+    ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
+    {
+        GLuint vertexShaderID;
+        if (!createShader(vertexShader, GL_VERTEX_SHADER, vertexShaderID))
+        {
+            std::cerr << "VERTEX SHADER compile-time error" << std::endl;
+            return;
+        }
 
-		m_ID = glCreateProgram();
-		glAttachShader(m_ID, vertexShaderID);
-		glAttachShader(m_ID, fragmentShaderID);
-		glLinkProgram(m_ID);
+        GLuint fragmentShaderID;
+        if (!createShader(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderID))
+        {
+            std::cerr << "FRAGMENT SHADER compile-time error" << std::endl;
+            glDeleteShader(vertexShaderID);
+            return;
+        }
 
-		GLint success;
-		glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
-		if (!success)
-		{
-			GLchar infolog[1024];
-			glGetShaderInfoLog(m_ID, 1024, nullptr, infolog);
-			std::cout << "ERROR::SHADER: Link-time error: \n" << infolog << std::endl;
-		}
-		else
-		{
-			m_isCompiled = true;
-		}
+        m_ID = glCreateProgram();
+        glAttachShader(m_ID, vertexShaderID);
+        glAttachShader(m_ID, fragmentShaderID);
+        glLinkProgram(m_ID);
 
-		glDeleteShader(vertexShaderID);
-		glDeleteShader(fragmentShaderID);
-	}
-	bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID)
-	{
-		shaderID = glCreateShader(shaderType);
-		const char* code = source.c_str();
-		glShaderSource(shaderID, 1, &code, nullptr);
-		glCompileShader(shaderID);
+        GLint success;
+        glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            GLchar infoLog[1024];
+            glGetShaderInfoLog(m_ID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: Link-time error:\n" << infoLog << std::endl;
+        }
+        else
+        {
+            m_isCompiled = true;
+        }
 
-		GLint success;
-		glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			GLchar infolog[1024];
-			glGetShaderInfoLog(shaderID, 1024, nullptr, infolog);
-			std::cout << "ERROR::SHADER: Compile-time error: \n" << infolog << std::endl;
-			return false;
-		}
-		return true;
-	}
+        glDeleteShader(vertexShaderID);
+        glDeleteShader(fragmentShaderID);
+    }
 
-	ShaderProgram::~ShaderProgram()
-	{
-		glDeleteProgram(m_ID);
-	}
 
-	void ShaderProgram::use() const
-	{
-		glUseProgram(m_ID);
-	}
+    bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID)
+    {
+        shaderID = glCreateShader(shaderType);
+        const char* code = source.c_str();
+        glShaderSource(shaderID, 1, &code, nullptr);
+        glCompileShader(shaderID);
 
-	ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram)
-	{
-		glDeleteProgram(m_ID);
-		m_ID = shaderProgram.m_ID;
-		m_isCompiled = shaderProgram.m_isCompiled;
+        GLint success;
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            GLchar infoLog[1024];
+            glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: Compile-time error:\n" << infoLog << std::endl;
+            return false;
+        }
+        return true;
+    }
 
-		m_ID = shaderProgram.m_ID = 0;
-		shaderProgram.m_isCompiled = false;
-		return *this;
-	}
-	ShaderProgram::ShaderProgram(ShaderProgram&& shaderProgram)
-	{
-		m_ID = shaderProgram.m_ID;
-		m_isCompiled = shaderProgram.m_isCompiled;
+    ShaderProgram::~ShaderProgram()
+    {
+        glDeleteProgram(m_ID);
+    }
 
-		m_ID = shaderProgram.m_ID = 0;
-		shaderProgram.m_isCompiled = false;
-	}
+    void ShaderProgram::use() const
+    {
+        glUseProgram(m_ID);
+    }
+
+    ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram) noexcept
+    {
+        glDeleteProgram(m_ID);
+        m_ID = shaderProgram.m_ID;
+        m_isCompiled = shaderProgram.m_isCompiled;
+
+        shaderProgram.m_ID = 0;
+        shaderProgram.m_isCompiled = false;
+        return *this;
+    }
+
+    ShaderProgram::ShaderProgram(ShaderProgram&& shaderProgram) noexcept
+    {
+        m_ID = shaderProgram.m_ID;
+        m_isCompiled = shaderProgram.m_isCompiled;
+
+        shaderProgram.m_ID = 0;
+        shaderProgram.m_isCompiled = false;
+    }
 }
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
 
 GLfloat point[] =
 {
@@ -16,24 +17,6 @@ GLfloat colors[] =
     0.0f, 1.0f, 0.0f,
     0.0f, 0.0f, 1.0f
 };
-
-const char* vertex_shader =
-"#version 460\n"
-"layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color;"
-"out vec3 color;"
-"void main() {"
-"   color = vertex_color;"
-"   gl_Position = vec4(vertex_position, 1.0);"
-"}";
-
-const char* fragment_shader =
-"#version 460\n"
-"in vec3 color;"
-"out vec4 frag_color;"
-"void main() {"
-"   frag_color = vec4(color, 1.0);"
-"}";
 
 
 int g_windowSizeX = 640;
@@ -54,7 +37,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
     }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     
     /* Initialize the library */
@@ -92,57 +75,57 @@ int main(void)
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     
     glClearColor(1, 1, 0, 1);
-
-    std::string vertexShader(vertex_shader);
-    std::string fragmentShader(fragment_shader);
-    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
-    if (!shaderProgram.isCompiled())
     {
-        std::cout << "Can`t create shader program!" << std::endl;
-        return -1;
-    }
+        ResourceManager resourceManager(argv[0]);
+        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+        if (!pDefaultShaderProgram)
+        {
+            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            return -1;
+        }
 
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
 
-    GLuint points_vbo = 0;
-    glGenBuffers(1, &points_vbo);    //Drivere generate one vertex buffer object and write identificator  value in &points_vbo
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
-    GLuint colors_vbo = 0;
-    glGenBuffers(1, &colors_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        shaderProgram.use();
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3); //draw current bindvertexarrayobject
 
-        /* Swap front and back buffers */ 
-        //Renderer draw a buffer,frame after next monitor update buffer show at the monitor
-        //2 buffers : front and back, Randerer draw at the back buffer,monitor show only front buffer
-        //When we used glfwSwapBuffer buffers swaps,but the frame at the back buffer are finished and
-        //the image from back buffer cannot be changed at the next timeframe
-        glfwSwapBuffers(pWindow);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(pWindow))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            pDefaultShaderProgram->use();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3); //draw current bindvertexarrayobject
+
+            /* Swap front and back buffers */
+            //Renderer draw a buffer,frame after next monitor update buffer show at the monitor
+            //2 buffers : front and back, Randerer draw at the back buffer,monitor show only front buffer
+            //When we used glfwSwapBuffer buffers swaps,but the frame at the back buffer are finished and
+            //the image from back buffer cannot be changed at the next timeframe
+            glfwSwapBuffers(pWindow);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
     }
     //Releasing resouses
     glfwTerminate();
